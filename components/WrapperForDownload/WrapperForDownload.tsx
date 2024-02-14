e3fc784fd9845c4b868877024144c73e770ec598
vladyslav-kurmaz <vladyslavkurmaz@gmail.com>
2024-02-15 00:24:33 +0200

    fix: add cheak request

--- components/WrapperForDownload/WrapperForDownload.tsx
+++ components/WrapperForDownload/WrapperForDownload.tsx
@@ -119,26 +119,33 @@ const WrapperForDownload = () => {
     }
   }, [url]);
 
+  type ErrorT = {
+    message: string,
+    status: number
+  }
+
   const getVideo = async (url: string) => {
     setLoading(true);
     try {
       const postRequest = await getVideoInfo(url);
       // console.log(postRequest.message);      
 
-      if (postRequest.message.includes("There is no supporting for")) {
+      if (postRequest.message && postRequest?.message.includes("There is no supporting for")) {
+        console.log('test');
+        
         localStorage.setItem("errorDontSupport", "true");
         router.push(`/`);
         return;
       }
 
-      if (postRequest.message === "Timeout error for worker") {
+      if (postRequest?.message === "Timeout error for worker") {
         
         localStorage.setItem("errorLongRequest", "true");
         router.push(`/`);
         return;
       }
 
-      if (postRequest.message === "Can not find meta for this URL") {
+      if (postRequest?.message === "Can not find meta for this URL") {
         localStorage.setItem("errorNotFindVideo", "true");
         router.push(`/`);
         return;
